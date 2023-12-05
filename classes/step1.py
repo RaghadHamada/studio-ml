8581035bcc23edb61265f87475c161c6c813d1d2
RaghadHamada <149780055+RaghadHamada@users.noreply.github.com>
2023-12-05 14:29:11 +0100

    Create step1.py

---
+++ classes/step1.py
@@ -0,0 +1,8 @@
+from step import Step
+import pandas as pd
+
+class Step1(Step):
+    def process(self, data) -> pd.DataFrame:
+        # Implement the processing logic for Step1 here
+        pass
+
